37cd57166d907cdc903b6a2191333253b76e19c9
Dushan Randika <DushanR@millenniumitesp.com>
2019-01-06 20:47:20 +0530

    Added all the file to Electron and enabled hot reloading

---
+++ test.jsx
@@ -0,0 +1,14 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+
+// ReactDOM.render(
+//     <div>
+// <div>Hi</div>
+// <div>Hi</div>
+// <div>Hi</div>
+// <div>Hi</div>
+// <div>Hi</div>
+
+//     </div>
+
+// , document.getElementById('root'));
